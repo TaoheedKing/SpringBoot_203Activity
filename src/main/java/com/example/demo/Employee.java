35c99c43f923844d7f136168c74ee87d129b7f3b
TaoheedKing <tking92@montgomerycollege.edu>
2020-05-20 15:34:38 -0400

    Created a SpringBoot form page including sumit button and page

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private long ssn;
+    private int month;
+    private int day;
+    private int year;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(long ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/main/java/com/example/demo/SpringBoot203ExerciseAApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot203ExerciseAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot203ExerciseAApplication.class, args);
+    }
+
+}
